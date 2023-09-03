9648d61263b471ac2f7a6b3f166a0f5b3934fb01
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-09-03 19:25:26 +0300

    refactor: Remove unnecessary documentation

--- cmd/api-gateway/main.go
+++ cmd/api-gateway/main.go
@@ -6,15 +6,6 @@ import (
 	"log"
 )
 
-// @title Wakarimi ApiGateway API
-// @version 0.1
-// @description This is the api gateway service for Wakarimi.
-// @contact.name Zalimannard
-// @contact.email zalimannard@mail.ru
-// @license.name MIT
-// @license.url https://opensource.org/licenses/MIT
-// @host localhost:8021
-// @BasePath /api/api-gateway-service
 func main() {
 	cfg, err := config.LoadConfiguration()
 	if err != nil {
