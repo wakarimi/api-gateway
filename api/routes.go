9aac9fd3a8a492b406d00bb0bb44b01ae96db4c1
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-12-05 23:33:08 +0500

    feat: Redirection to leave the room

--- api/routes.go
+++ api/routes.go
@@ -95,6 +95,7 @@ func SetupRouter(ac *context.AppContext) (r *gin.Engine) {
 			musicPlayback.POST("/rooms/join", musicPlaybackHandler.ProxyRequest("/rooms/join", util.StrPtr("USER")))
 			musicPlayback.POST("/rooms", musicPlaybackHandler.ProxyRequest("/rooms", util.StrPtr("USER")))
 			musicPlayback.PATCH("/rooms/:roomId/rename", musicPlaybackHandler.ProxyRequest("/rooms/:roomId/rename", util.StrPtr("USER")))
+			musicPlayback.DELETE("/rooms/:roomId/leave", musicPlaybackHandler.ProxyRequest("/rooms/:roomId/leave", util.StrPtr("USER")))
 
 			musicPlayback.GET("/rooms/:roomId/share-code", musicPlaybackHandler.ProxyRequest("/rooms/:roomId/share-code", util.StrPtr("USER")))
 			musicPlayback.POST("/rooms/:roomId/share-code", musicPlaybackHandler.ProxyRequest("/rooms/:roomId/share-code", util.StrPtr("USER")))
